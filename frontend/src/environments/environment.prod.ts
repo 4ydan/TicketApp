175f1bb6c7cdc896b2ee88992cc479d8c52b3132
Aydan Namdar Ghazani <e11709245@student.tuwien.ac.at>
2023-06-05 18:08:10 +0200

    firebase config

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true
+  firebase: {
+    apiKey: "AIzaSyDW15Nc1VC8i5tfHH8ICKWBRVRItfb0eik",
+    authDomain: "filmer-920e0.firebaseapp.com",
+    projectId: "filmer-920e0",
+    storageBucket: "filmer-920e0.appspot.com",
+    messagingSenderId: "968915815982",
+    appId: "1:968915815982:web:bbf9b71a43682e74c2d9bf",
+    measurementId: "G-MVZSPWCT03"
+  }
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDW15Nc1VC8i5tfHH8ICKWBRVRItfb0eik",
+    authDomain: "filmer-920e0.firebaseapp.com",
+    projectId: "filmer-920e0",
+    storageBucket: "filmer-920e0.appspot.com",
+    messagingSenderId: "968915815982",
+    appId: "1:968915815982:web:bbf9b71a43682e74c2d9bf",
+    measurementId: "G-MVZSPWCT03"
+  }
 };
 
 /*
